e1eb98d67599e01ec9f4d9f0d36f19399da02ccd
SHUIJINGSHI2 <ZHOUYAOYONG533@pingan.com>
2014-05-14 19:06:25 +0800

    首次提交到本地仓库

---
+++ src/com/pingan/HelloTrust.java
@@ -0,0 +1,12 @@
+package com.pingan;
+
+import java.util.Date;
+
+public class HelloTrust {
+
+	public static void main(String[] args) {
+		System.out.println("hello,trust!");
+		System.out.println(new Date());
+	}
+
+}
